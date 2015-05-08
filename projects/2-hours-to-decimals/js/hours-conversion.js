ef55729b8f2f7fb1823525cf333b8f05155ed879
jessabean <jessica.avison@gmail.com>
2015-05-08 12:46:15 -0400

    [day 2] Alert the total of the two inputs

---
+++ projects/2-hours-to-decimals/js/hours-conversion.js
@@ -0,0 +1,25 @@
+function convertMinutes() {
+  var $form           = $('.js-time-conversion-form');
+  var $hoursInput     = $('.js-hours-input');
+  var $minutesInput   = $('.js-minutes-input');
+
+  $form.submit(function( event ) {
+    var hours             = $hoursInput.val();
+    var minutes           = $minutesInput.val();
+
+    var minutesInDecimal  = (minutes / 60).toFixed(2)
+    
+    var convertedHours    = parseFloat(hours);
+    var convertedMinutes  = parseFloat(minutesInDecimal);
+    var total             = convertedHours += convertedMinutes;
+
+    alert(total);
+
+    event.preventDefault();
+  });
+};
+
+$(document).ready(function() {
+  convertMinutes();
+});
+  
