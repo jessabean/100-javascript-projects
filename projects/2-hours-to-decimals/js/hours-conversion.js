7d5504f8b854e82656a8bc6aa0c4b9215b49f8b8
jessabean <jessica.avison@gmail.com>
2015-05-09 23:18:47 -0400

    [day 2] Calculate converted total on input change

--- projects/2-hours-to-decimals/js/hours-conversion.js
+++ projects/2-hours-to-decimals/js/hours-conversion.js
@@ -1,30 +1,37 @@
+var $form           = $('.js-time-conversion-form');
+var $hoursInput     = $('.js-hours-input');
+var $minutesInput   = $('.js-minutes-input');
+var $decimalOutput  = $('.js-converted-hours');
+
+
 function convertMinutes() {
-  var $form           = $('.js-time-conversion-form');
-  var $hoursInput     = $('.js-hours-input');
-  var $minutesInput   = $('.js-minutes-input');
-  var $decimalOutput  = $('.js-converted-hours');
+  var hours             = $hoursInput.val();
+  var minutes           = $minutesInput.val();
 
-  $form.submit(function( event ) {
-    var hours             = $hoursInput.val();
-    var minutes           = $minutesInput.val();
+  if( hours == "" ) {
+    hours = "0";
+  }
 
-    if( hours == "" ) {
-      hours = "0";
-    }
+  var minutesInDecimal  = (minutes / 60).toFixed(2)
+  
+  var convertedHours    = parseFloat(hours);
+  var convertedMinutes  = parseFloat(minutesInDecimal);
+  var total             = convertedHours += convertedMinutes;
 
-    var minutesInDecimal  = (minutes / 60).toFixed(2)
-    
-    var convertedHours    = parseFloat(hours);
-    var convertedMinutes  = parseFloat(minutesInDecimal);
-    var total             = convertedHours += convertedMinutes;
+  $decimalOutput.html(total + " hours");
+};
 
-    $decimalOutput.html(total + " hours");
-
-    event.preventDefault();
+function calculateMinutes() {
+  $hoursInput.on('change', function() {
+    convertMinutes();
   });
+  $minutesInput.on('change', function() {
+    convertMinutes();
+  })
 };
 
 $(document).ready(function() {
   convertMinutes();
+  calculateMinutes();
 });
   
