011427dec258497d811784db87834fa12f6d3634
jessabean <jessica.avison@gmail.com>
2015-05-24 12:09:58 -0400

    [day 4] Add fahrenheit to celsius math

--- projects/4-temperature-converter/js/temperature-conversion.js
+++ projects/4-temperature-converter/js/temperature-conversion.js
@@ -1,4 +1,21 @@
+function fahrenheitToCelsius(value) {
+  // Fahrenheit to Celsius math
+  var celsiusConversionFormula = (value - 32) * 5/9;
+
+  // limit to one decimal place
+  var celsiusOutput = (celsiusConversionFormula).toFixed(1);
+
+  $('.js-input-celsius').val(celsiusOutput);
+};
+
 $ ( document ).ready(function() {
   $inputFahrenheit = $('.js-input-fahrenheit');
   $inputCelsius = $('.js-input-celsius');
+
+  $inputFahrenheit.on('blur', function() {
+    var $fahrenheitValue = $inputFahrenheit.val();
+
+    fahrenheitToCelsius($fahrenheitValue);
+  });
+  
 });
