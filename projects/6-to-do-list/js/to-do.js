9e269a616c0ddca42c6b611419ac9fc0a53ca394
jessabean <jessica.avison@gmail.com>
2016-05-18 16:44:07 -0400

    [day 6] Add click handler to generated todo items

--- projects/6-to-do-list/js/to-do.js
+++ projects/6-to-do-list/js/to-do.js
@@ -8,14 +8,24 @@ var addToDo = function() {
   var txt = newToDo.value;
   toDoArray.push(txt);
 
-  var items = '<li>' + toDoArray.join('</li><li>') + '</li>';
+  var items = '<li class="todo__item">' + toDoArray.join('</li><li class="todo__item">') + '</li>';
   toDoList.innerHTML = items;
   newToDo.value = '';
+
+  var toDos = document.querySelectorAll('li.todo__item');
+
+  var completeToDo = function() {
+    this.classList.toggle("todo__item--complete");
+  };
+
+  for(var i = 0; i < toDos.length; i++) {
+    toDos[i].addEventListener('click', completeToDo, false);
+  }
 };
 
 
 document.getElementById('btn-submit').addEventListener(
-  'click', 
+  'click',
   function(event){
     event.preventDefault();
     addToDo();
